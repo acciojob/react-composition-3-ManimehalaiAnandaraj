57b97e8f262669c51c510bb5a5471a6bb992bbe1
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-19 14:29:00 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
-import React from "react";
+import React from 'react';
+import Tooltip from './Tooltip';
 import './../styles/App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <h1>Tooltip Demo</h1>
+      
+      <Tooltip text="This is a tooltip">
+        <button>Hover over me</button>
+      </Tooltip>
+
+      <br /><br />
+
+      <Tooltip text="This is another tooltip">
+        <span style={{ color: 'blue', textDecoration: 'underline' }}>
+          Hover over me to see another tooltip
+        </span>
+      </Tooltip>
+
+     
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Tooltip.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './../styles/tooltip.css';
+
+const Tooltip = ({ text, children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const showTooltip = () => setIsVisible(true);
+  const hideTooltip = () => setIsVisible(false);
+
+  return (
+    <div className="tooltip-container">
+      <div 
+        className="tooltip" 
+        onMouseEnter={showTooltip}
+        onMouseLeave={hideTooltip}
+      >
+        {children}
+      </div>
+      {isVisible && (
+        <div className="tooltiptext">
+          {text}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Tooltip;
