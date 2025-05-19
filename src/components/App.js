2c68fa0b2f22f518d5844ce71218bbe869585400
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-19 16:14:38 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,22 @@ import './../styles/App.css';
 function App() {
   return (
     <div className="App">
+      <div>
       <Tooltip text="This is a tooltip">
         <h2>Hover over me</h2>
       </Tooltip>
-
-      <br /><br />
-
+      </div>
+      <div>
       <Tooltip text="This is another tooltip">
         <p>
           Hover over me to see another tooltip
         </p>
       </Tooltip>
+    
 
       <Tooltip text="Information about this icon">
         <span style={{ fontSize: '24px', cursor: 'pointer' }}>🔍</span> {/* Added an icon */}
-      </Tooltip>
+      </Tooltip></div>
     </div>
   );
 }
