1ad46661391eecd04cd30efb9ad1d4c54d468c76
manimehalaianandaraj <manimehalaiannakodi@gmail.com>
2025-05-21 11:16:58 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,19 @@ function App() {
   return (
     <div className="App">
       
-            <div><h2>
+   <div>
+    
+    <h2>
      <Tooltip text="This is a tooltip">
         Hover over me 
       </Tooltip></h2>
-      </div>
 
-      <div><p>
+      <p>
       <Tooltip text="This is another tooltip">
-        
           Hover over me to see another tooltip
-       
       </Tooltip> </p>
-    </div>
     
+</div>
     </div>
   );
 }
